20048f9b00ce917a82bf29d634e77dcc30333bad
moaabid <moaabid153@gmail.com>
2022-03-17 09:29:42 +0530

    [Add] Valuenotifier

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+      routes: {
+        '/addcontact': (context) => const NewContactView(),
+      },
+    );
+  }
+}
+
+class Contact {
+  final String name;
+  final String id;
+  Contact({
+    required this.name,
+  }) : id = const Uuid().v4();
+}
+
+class ContactBook extends ValueNotifier<List<Contact>> {
+  ContactBook._sharedInstance() : super([]);
+
+  static final ContactBook _shared = ContactBook._sharedInstance();
+
+  factory ContactBook() => _shared;
+
+  int get lengthOfContact => value.length;
+
+  void add({required Contact contact}) {
+    value.add(contact);
+    notifyListeners();
+  }
+
+  void remove({required Contact contact}) {
+    value.remove(contact);
+    notifyListeners();
+  }
+
+  Contact? getContact({required int atIndex}) =>
+      value.length > atIndex ? value.elementAt(atIndex) : null;
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Contact Page")),
+      body: ValueListenableBuilder(
+        builder: (BuildContext context, value, Widget? child) {
+          final contacts = value as List<Contact>;
+          return ListView.builder(
+            itemCount: contacts.length,
+            itemBuilder: (BuildContext context, int index) {
+              final contact = contacts[index];
+              return Dismissible(
+                onDismissed: (direction) {
+                  ContactBook().remove(contact: contact);
+                },
+                key: ValueKey(contact.id),
+                child: Material(
+                  color: Colors.white,
+                  elevation: 6.0,
+                  child: ListTile(
+                    title: Text(contact.name),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        valueListenable: ContactBook(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/addcontact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({Key? key}) : super(key: key);
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final TextEditingController _contactText;
+
+  @override
+  void initState() {
+    _contactText = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _contactText.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Add New Contact"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextField(
+                controller: _contactText,
+                decoration: const InputDecoration(
+                  hintText: "Enter new contact name",
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    final contact = Contact(name: _contactText.text);
+
+                    ContactBook().add(contact: contact);
+
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Add Contact'))
+            ],
+          ),
+        ));
+  }
+}
